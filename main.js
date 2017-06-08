90f9537473b7af7d381b3a812b96527d8502c6f7
Dylan Myers <sunshine@Dylans-MacBook-Air.local>
2017-06-08 16:23:22 -0400

    second commit

--- main.js
+++ main.js
@@ -5,18 +5,23 @@ title.textContent = 'Sign up for my web app';
 var container = document.querySelector('#container');
 container.appendChild(title);
 
-for ( var i = 0; i < formData.length; i++){
-    var control = formData[i];
-var input = document.createElement('input');
-input.id = control.id;
-input.type = control.type;
-input.placeholder = control.label;
-var label = document.createElement("label");
-    label.setAttribute('for', control.id);
-    container.appendChild(label);
-    container.appendChild(input);
-    if(input.type === "email"){
-        
-}
-container.appendChild(input)
-}
+for (var i = 0; i <formData.length; i++) {
+
+ var control = formData[i];
+  if (control.type === "textarea") {
+    var inputElement = document.createElement('textarea');
+
+ }else if (control.type === "select") {
+    var inputElement = document.createElement('select');
+
+ }
+  else {
+    var inputElement = document.createElement('input');
+
+ }
+
+ inputElement.id = control.id;
+  inputElement.type = control.type;
+  inputElement.placeholder = control.label;
+  container.appendChild(inputElement);
+  }
